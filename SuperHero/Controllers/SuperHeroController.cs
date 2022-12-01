0f4081b159a72262742f8e1811183ee815fce501
adriand <adriand@l2b.co.za>
2022-12-01 13:09:27 +0200

    Add project files.

---
+++ SuperHero/Controllers/SuperHeroController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHero.Data;
+using SuperHero.Models;
+
+namespace SuperHero.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SuperHeroController : ControllerBase {
+		private readonly DataContext _context;
+
+		public SuperHeroController(DataContext context) {
+			_context = context;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<List<SuperHeros>>> Get() {
+			return Ok(await _context.SuperHeroes.ToListAsync());
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<SuperHeros>> Get(int id) {
+			var hero = await _context.SuperHeroes.FindAsync(id);
+			if(hero == null)
+				return BadRequest("Hero not found.");
+			return Ok(hero);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<List<SuperHeros>>> AddHero(SuperHeros hero) {
+			_context.SuperHeroes.Add(hero);
+			await _context.SaveChangesAsync();
+
+			return Ok(await _context.SuperHeroes.ToListAsync());
+		}
+
+		[HttpPut]
+		public async Task<ActionResult<List<SuperHeros>>> UpdateHero(SuperHeros request) {
+			var dbHero = await _context.SuperHeroes.FindAsync(request.Id);
+			if(dbHero == null)
+				return BadRequest("Hero not found.");
+
+			dbHero.Name = request.Name;
+			dbHero.FirstName = request.FirstName;
+			dbHero.LastName = request.LastName;
+			dbHero.Place = request.Place;
+
+			await _context.SaveChangesAsync();
+
+			return Ok(await _context.SuperHeroes.ToListAsync());
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<ActionResult<List<SuperHeros>>> Delete(int id) {
+			var dbHero = await _context.SuperHeroes.FindAsync(id);
+			if(dbHero == null)
+				return BadRequest("Hero not found.");
+
+			_context.SuperHeroes.Remove(dbHero);
+			await _context.SaveChangesAsync();
+
+			return Ok(await _context.SuperHeroes.ToListAsync());
+		}
+	}
+}
---
+++ SuperHero/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using SuperHero.Models;
+
+namespace SuperHero.Data {
+	public class DataContext : DbContext {
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+		public DbSet<SuperHeros> SuperHeroes { get; set; }
+	}
+}
